4b8def2396e7793bfd80ee926ca51ef577e1bb1a
soriee <soriee@126.com>
2019-12-29 21:40:58 +0800

    feat(*): 项目初始化

---
+++ src/main/java/com/unicorn/OutsApplication.java
@@ -0,0 +1,13 @@
+package com.unicorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicorn/OutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicorn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
